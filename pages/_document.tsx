2d152b61530d9b74f7f5bdc7ee82bd2ba65caf37
{fujitanao} <fujitanao.rmt@gmail.com>
2022-10-09 20:36:52 +0900

    Initial Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,46 @@ type Props = {}
 class Document extends NextDocument<Props> {
   render() {
     return (
-      <Html lang="ja">
-        <Head />
+      <Html lang="ja">     
+      <title>コロナ感染者数観測アプリ</title>
+      <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+
+      <link rel="shortcut" href="../../public/favicon.ico" />
+
+      <meta charSet="utf-8" />
+
+      <meta name="description" content="ポートフォリオ用のサイトです。各国のコロナ感染者を観測するサイトです。" />
+      <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1, width=device-width, initial-scale=1" />
+      <meta name="keywords" content="コロナ ポートフォリオ" />
+      <meta name="google" content="nositelinkssearchbox" />
+
+        </Head>
         <body>
           <Main />
           <NextScript />
