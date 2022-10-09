7cd609b3f1785a0e67613b01bba99ef03ba1c072
{fujitanao} <fujitanao.rmt@gmail.com>
2022-10-09 20:47:06 +0900

    Initial Commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+
+      </main>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import styles from './Layout.module.scss'
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+
+      </main>
+
+    </div>
+  )
+}
+
+export default Layout
